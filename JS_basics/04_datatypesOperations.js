655dbb8bfa1f346f84b81e963561ef5f512a8daf
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2023-09-18 14:24:34 +0000

    datatypes operations

---
+++ JS_basics/04_datatypesOperations.js
@@ -0,0 +1,9 @@
+let str1 = "hello"
+let str2 =" Shubhra"
+let str3 = str1 + str2
+//console.log(str3)
+console.log(1 +"2")
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2 +2)// string in first so treated as string
+console.log(1+2+"3") //string in last so the first two numbers will treated as numbers and they will sum up
