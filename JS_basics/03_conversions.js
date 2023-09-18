89f77956abe935bfdd8353216429fadf3ab5d904
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2023-09-18 14:03:52 +0000

    conversion of datatypes

---
+++ JS_basics/03_conversions.js
@@ -0,0 +1,14 @@
+let score = "33"
+//console.log(typeof score)
+let valueInNum= Number(score);
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+let loggedIn = 0
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(booleanLoggedIn)
+/* 1= true
+0= false
+""=false
+"shubhra"= true
+*/
