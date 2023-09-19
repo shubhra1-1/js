7a7e214e404ac4956153a08c8592753afbf34163
shu-bee <125111194+shubhra1-1@users.noreply.github.com>
2023-09-19 08:32:51 +0000

    maths

---
+++ JS_basics/08_maths.js
@@ -0,0 +1,16 @@
+// const balance = new Number(200)
+// console.log(balance)
+// const score= 100
+// console.log(score)
+//  console.log(balance.toString())
+// console.log(balance.toFixed(3))
+// +++++++++++++++++++MATHS++++++++++++++++
+// console.log(Math.abs(-98))
+// console.log(Math.round(-98.7))
+// console.log(Math.floor(0.8))
+// console.log(Math.ceil(0.8))
+
+
+const min=90
+const max=100
+console.log(Math.floor(Math.random() * (max-min +1)+min))
